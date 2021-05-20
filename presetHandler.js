eb1d2011dbd24b8941a258d008dc8b4335f0fa87
Adam Marsala <adammarsala@hotmail.com>
2021-05-20 16:55:12 -0400

    Implement presetHandler for GET and PUT, handles edge cases and bad requests

--- presetHandler.js
+++ presetHandler.js
@@ -3,7 +3,29 @@ const presets = require('./presets');
 
 // Complete this function:
 const presetHandler = (requestType, presetIndex, newPresetArray) => {
-  return [];
+  // returns a status code and a presetArray for GET requests, e.g. [200, [true, true ...]]
+  // returns a status code and an updated presetArray for PUT requests [200, newPresetArray]
+  const validIndex = presets[presetIndex];
+  const presetArray = validIndex ? [200] : [404];
+
+  switch (requestType) {
+    case 'GET':
+      // add the requested presetArray to the returned result
+      if (validIndex) presetArray.push(presets[presetIndex]);
+    case 'PUT':
+      // if third argument is given, it will be a PUT request
+      if (newPresetArray) {
+        // update the presets array at the given index
+        presets[presetIndex] = newPresetArray;
+        // add the new array to the returned preset
+        presetArray.push(newPresetArray);
+      }
+      // always return here for GET or PUT, no break statements.
+      return presetArray;
+    default: 
+      // invalid/bad request
+      return [400]
+  }
 };
 
 // Leave this line so that your presetHandler function can be used elsewhere:
